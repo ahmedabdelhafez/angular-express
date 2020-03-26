2f9c39dbeb4cc5289a5940b54f213f9e973030ed
ahmedabdelhafez <messi10010@gmail.com>
2020-03-26 18:23:21 +0200

    add files
    add files and packages

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const path = require("path");
 import express from "express";
+import dotenv from "dotenv";
 
+dotenv.config();
+///////
 const app = express();
+
+// << load angular app index.html file >> //
 app.use(express.static(__dirname + "/dist"));
 app.get("/*", function(req, res) {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "/dist", "index.html"));
 });
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080, () => {
+  console.log("app run well");
+});
